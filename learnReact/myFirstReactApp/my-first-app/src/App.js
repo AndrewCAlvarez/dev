59975ebee4ef01d6a2d7a8b797811b7afb06f869
Andrew <alvaandrew2@gmail.com>
2019-10-13 19:29:57 -0400

    my own react app practice

---
+++ learnReact/myFirstReactApp/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TonyStark } from "./components/TonyStark";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
---
+++ learnReact/myFirstReactApp/my-first-app/src/components/TonyStark.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function TonyStark() {
+    return <div className="TonyStark">I am Iron Man</div>;
+}
