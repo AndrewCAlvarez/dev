837896c42ff990f3d5c298021310d33d1c000957
Andrew <alvaandrew2@gmail.com>
2019-12-08 04:31:42 -0500

    Update routes.js

--- game-night-app/server/routes/api/routes.js
+++ game-night-app/server/routes/api/routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 //  import other routers
 router.use(require("./playerCharacterRoutes"));
 router.use(require("./loginRoutes"));
-router.use(require("./chat"));
+// router.use(require("./chat"));
 
 module.exports = router;
