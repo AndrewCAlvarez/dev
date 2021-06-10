331585a85176b0b314af9d7006d889a111d92088
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2021-06-10 16:11:47 -0400

    Added react-bootstrap
    Working out how bootstrap is used in react.

--- Websites/Cleq/cleq/src/App.js
+++ Websites/Cleq/cleq/src/App.js
@@ -5,17 +5,32 @@ import Anime from 'react-anime';
 import { Nav } from './Components/Nav';
 import {BodyContent} from './Components/BodyContent';
 import {Footer} from './Components/Footer';
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-        <Nav />
-        <Anime opacity={[0,1]} easing={'easeInCirc'}>
-          <BodyContent/>
-        </Anime>
-        <Footer />
+      
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      React Bootstrap
+    </Navbar.Brand>
+  </Navbar>
+
+      <Nav />
+        <BodyContent/>
+      <Footer />
+
     </div>
   );
 }
