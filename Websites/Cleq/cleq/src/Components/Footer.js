ba4d1a6246bd173ab06f7c0da03e963250fabbe2
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2021-06-12 03:59:02 -0400

    resizing logo svgs

--- Websites/Cleq/cleq/src/Components/Footer.js
+++ Websites/Cleq/cleq/src/Components/Footer.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {ReactComponent as MashableLogo} from '../mashable-logo.svg';
+import MashableLogo from '../mashable-logo.svg';
+import IndeedLogo from '../indeed-logo.svg';
+import OrangeLogo from '../orange-logo.svg';
+import AmazonLogo from '../amazon-logo.svg';
+
 
 export class Footer extends React.Component{
-
+    constructor(props){
+        super(props);
+        this.state = {
+            logoHeight : "5em",
+            logoWidth : "5em",
+        }
+    }
+    
     render(){
         return(
             <div className="Footer">
                 <div className="Footer-Content">
                     <h3>The world's top companies use Cleq to turn data into revenue</h3>   
-                    <MashableLogo/>
+                    <img src={MashableLogo} style={{ height: this.state.logoHeight, width: this.state.logoWidth }} alt="website logo"/>
+                    <img src={IndeedLogo} style={{ height: this.state.logoHeight, width: this.state.logoWidth }} alt="website logo"/>
+                    <img src={OrangeLogo} style={{ height: this.state.logoHeight, width: this.state.logoWidth }} alt="website logo"/>
+                    <img src={AmazonLogo} style={{ height: this.state.logoHeight, width: this.state.logoWidth }} alt="website logo"/>
                 </div>
             </div>
         )
