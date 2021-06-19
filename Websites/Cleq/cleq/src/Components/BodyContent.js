d6e2643246e6c58df368655bf4d70527f1fc6e3a
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2021-06-19 11:26:18 -0400

    added blob svgs

--- Websites/Cleq/cleq/src/Components/BodyContent.js
+++ Websites/Cleq/cleq/src/Components/BodyContent.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import {ArrowUpRight} from 'react-bootstrap-icons'
 import Grid from '../grid.svg';
+import SigmaBlob from '../sigma-blob.svg';
+import DeltaBlob from '../delta-blob.svg';
+import EqualsBlob from '../equals-blob.svg';
+import IntegralBlob from '../integral-blob.svg';
+import InfinityBlob from '../infinity-blob.svg';
+import BlackBlob from '../black-blob.svg';
+
 
 export class BodyContent extends React.Component{
     constructor(props){
@@ -9,6 +16,7 @@ export class BodyContent extends React.Component{
         this.state = {
             gridHeight : "60vh",
             gridWidth : "100%",
+            sigmaBlobDimensions: "width: 50px; height:50px;",
         }
     }
 
@@ -18,7 +26,11 @@ export class BodyContent extends React.Component{
                 {/* Grid svg */}
                 {/* <img id="body-content__grid" src={Grid} style={{ height: this.state.gridHeight, width: this.state.gridWidth }} /> */}
                 <div className="body-content">
-                <div></div>
+                <div>
+                    <img class="blob" src={EqualsBlob} />
+                    <img class="blob" src={BlackBlob} />
+                    <img class="blob" src={DeltaBlob} />
+                </div>
                 <div className="body-content-center">
                     <h1>PLAY</h1>
                     <h1>WITH</h1>
@@ -26,7 +38,12 @@ export class BodyContent extends React.Component{
                     <h1>DATA</h1>
                     <button href="#">GET A DEMO <ArrowUpRight></ArrowUpRight></button>
                 </div>
-                <div></div>
+                <div>
+                    <img class="blob" src={BlackBlob} />
+                    <img class="blob" src={IntegralBlob} />
+                    <img class="blob" src={SigmaBlob} />
+                    <img class="blob" src={InfinityBlob} />
+                </div>
                 </div>
             </>
         )
