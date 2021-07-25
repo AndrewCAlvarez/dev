3e31ee0c79eadf33dd59f0d7dc5aff24892fd346
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2021-07-25 18:42:46 -0400

    f

--- testnodejs/api/app.js
+++ testnodejs/api/app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI.js');
 var app = express();
+app.use(cors()); // If i put app.use(cors()) after other statements lower than here, I get cors errors.
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- testnodejs/api/routes/testAPI.js
+++ testnodejs/api/routes/testAPI.js
@@ -2,12 +2,36 @@ const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
 
+var data = {
+    title: "Finances",
+    cost: 1200,
+    category: 900
+};
+
+var expenses = {
+    title: "",
+    cost: "",
+    category: ""
+}
+
 router.get('/', function(req, res){
-    res.send({
-        title: "Finances",
-        income: 900,
-        expenses: 900
-    });
+    console.log(expenses)
+    res.send(expenses);
 })
+
+router.post('/', function(req,res){
+    expenses = req.body.data;
+    console.log(expenses);
+})
+
+router.post('/delete', (req,res) => {
+    data.title = "";
+})
+
+router.post('/addExpense', (req, res) => {
+    expense = req.body;
+    console.log("Expenses: ", expenses);
+})
+
 
 module.exports = router;
